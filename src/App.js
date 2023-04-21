b3861c886da374d70803dba825e12cb2e78b9e39
Mansi <mansishah1697@gmail.com>
2023-04-21 19:39:44 +1000

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+
+import './style/App.scss';
+import "./style/Header.scss";
+import "./style/Home.scss";
+import "./style/Footer.scss";
+import "./style/contact.scss";
+import "./style/mediaQuery.scss";
+
+import Service from "./components/Service";
 
 function App() {
   return (
     <div className="App">
-     App
+    
+      <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/contact" element={<Contact />} /> 
+          <Route  path = "/services" element = {<Service/>} /> 
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
